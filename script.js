f972393dc72242ceabdf240babca90757a2a08a3
Piorek1994 <pprus94@gmail.com>
2022-04-13 17:59:36 +0200

    Function handleStop

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const historyBtn = document.querySelector('.history')
 const stopWatch = document.querySelector('.stopwatch')
 const time = document.querySelector('.time')
 const timeList = document.querySelector('.time-list')
-
 const infoBtn = document.querySelector('.info')
 const modalShadow = document.querySelector('.modal-shadow')
 const closeModalBtn = document.querySelector('.close')
@@ -15,8 +14,11 @@ const closeModalBtn = document.querySelector('.close')
 let countTime
 let minutes = 0
 let seconds = 0
+let timesArr = []
 
 const handleStart = () => {
+	clearInterval(countTime)
+
 	countTime = setInterval(() => {
 		if (seconds < 9) {
 			seconds++
@@ -32,10 +34,34 @@ const handleStart = () => {
 	}, 1000)
 }
 
-startBtn.addEventListener('click', handleStart)
+const handleStop = () => {
+
+    time.innerHTML = `Ostatni czas: ${stopWatch.textContent}`
+
+
+	if (stopWatch.textContent !== '0:00') {
+		time.style.visibility = 'visible'
+        timesArr.push(stopWatch.textContent)
+        
+	}
+
+	clearInterval(countTime)
+	stopWatch.textContent = '0:00'
+	timeList.textContent = ''
+	seconds = 0
+	minutes = 0
+}
+
+const handlePause = () => {
+	clearInterval(countTime)
+}
 
 const changeColor = () => {
 	document.documentElement.style.setProperty('--first-color', colorPicker.value)
 }
+
+pauseBtn.addEventListener('click', handlePause)
+startBtn.addEventListener('click', handleStart)
+stopBtn.addEventListener('click', handleStop)
 
 document.addEventListener('click', changeColor)
