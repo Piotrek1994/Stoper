7e74d2b638f7d3a4f31e27222a27b24c2642699f
Piorek1994 <pprus94@gmail.com>
2022-04-13 19:24:43 +0200

    Project finished

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const timeList = document.querySelector('.time-list')
 const infoBtn = document.querySelector('.info')
 const modalShadow = document.querySelector('.modal-shadow')
 const closeModalBtn = document.querySelector('.close')
+const brush = document.querySelector('.fa-solid')
 
 let countTime
 let minutes = 0
@@ -50,7 +51,7 @@ const handlePause = () => {
 
 const handleReset = () => {
 	time.style.visibility = 'hidden'
-    timesArr = []
+	timesArr = []
 	clearStuff()
 }
 
@@ -62,15 +63,47 @@ const clearStuff = () => {
 	minutes = 0
 }
 
+const showHistory = () => {
+	timeList.textContent = ''
+	let num = 1
 
+	timesArr.forEach(time => {
+		const newTime = document.createElement('li')
+		newTime.innerHTML = `Pomiar nr: ${num} <span>${time}</span>`
+		timeList.appendChild(newTime)
+		num++
+	})
+}
+
+const showModal = () => {
+	modalShadow.style.display = 'block'
+	modalShadow.classList.toggle('modal-animation')
+}
+const closeModal = () => {
+	modalShadow.style.display = 'none'
+}
 
 const changeColor = () => {
 	document.documentElement.style.setProperty('--first-color', colorPicker.value)
 }
 
+const changePicker = () => {
+	brush.style.color = '#fa1406'
+	brush.style.transition = 'all .5s'
+}
+const changePicker2 = () => {
+	brush.style.transition = 'all .5s'
+	brush.style.color = ''
+}
+
+colorPicker.addEventListener('mouseover', changePicker)
+colorPicker.addEventListener('mouseout', changePicker2)
 startBtn.addEventListener('click', handleStart)
 pauseBtn.addEventListener('click', handlePause)
 stopBtn.addEventListener('click', handleStop)
 resetBtn.addEventListener('click', handleReset)
+historyBtn.addEventListener('click', showHistory)
+infoBtn.addEventListener('click', showModal)
+closeModalBtn.addEventListener('click', closeModal)
 
-document.addEventListener('click', changeColor)
+window.addEventListener('click', changeColor)
